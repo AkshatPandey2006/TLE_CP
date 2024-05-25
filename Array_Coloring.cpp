5d1e5cc358f9aa6e16b8d163a66bc668c7ab73c5
Akshat Pandey <128710323+AkshatPandey2006@users.noreply.github.com>
2024-05-25 15:57:13 +0530

    Create Array_Coloring.cpp

---
+++ Array_Coloring.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;  
+    
+    while(t--) {
+        int n; 
+        cin >> n;
+        
+        int arr[n];
+        int cnt = 0;
+        for(int i=0; i<n; i++) {
+            cin >> arr[i];
+            if(arr[i] % 2 != 0) cnt++;
+        }
+        
+        if(cnt%2 == 0) cout << "Yes" << endl;
+        else cout << "No" << endl;
+    }
+}
